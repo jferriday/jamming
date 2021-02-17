17f7b7b3ea70503d679707a8e7c7f87ff78d997c
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2021-02-17 17:46:40 +0000

    directory structure and app style

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Playlist from "./Playlist/Playlist.js";
+import SearchBar from "./SearchBar/SearchBar.js";
+import SearchResults from "./SearchResults/SearchResults.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div id="app">
+          {/* <SearchBar /> */}
+          <div class="App-playlist">
+            {/* <SearchResults /> */}
+            {/* <Playlist /> */}
+          </div>
+        </div>
+      </div>
+    )
+}
+}
+
+export default App;
